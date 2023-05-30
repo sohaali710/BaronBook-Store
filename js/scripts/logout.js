e89b26d7991b14a41981b241b7a9d01d59002dae
Soha <sohaali710@gmail.com>
2023-05-30 19:24:55 +0300

    admin control panel design

--- js/scripts/logout.js
+++ js/scripts/logout.js
@@ -1,24 +1,26 @@
 import { getCookie, deleteCookie } from "./cookies.js";
 import { logInOutNav } from "./nav-log-in-out.js";
 
-let logOutBtn = document.getElementById('logout')
+const logOutBtn = document.getElementById('logout')
 
-let userToken = 'user_access_token'
-let adminToken = 'admin_access_token'
+const userToken = 'user_access_token'
+const adminToken = 'admin_access_token'
 
 let redirectTo = ''
 
 if (getCookie(userToken)) {
     logInOutNav(userToken)
-} else if (getCookie(adminToken)) {
-    logInOutNav(adminToken)
 }
+// else if (getCookie(adminToken)) {
+//     logInOutNav(adminToken)
+// }
 
 logOutBtn.addEventListener('click', event => {
     if (getCookie(userToken)) {
         deleteCookie(userToken)
         location.href = 'user-login.html'
     } else if (getCookie(adminToken)) {
+        console.log('here')
         deleteCookie(adminToken)
         location.href = 'admin-login.html'
     }
