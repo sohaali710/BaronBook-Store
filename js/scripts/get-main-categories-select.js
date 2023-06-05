97faff47f585fe1253da0ef95fe70a498575e937
Soha <sohaali710@gmail.com>
2023-06-05 22:18:58 +0300

    user add book

--- js/scripts/get-main-categories-select.js
+++ js/scripts/get-main-categories-select.js
@@ -1,27 +1,30 @@
 // in select [user (add book form)]
 import { getCookie } from "./cookies.js"
 
-const userToken = 'user_access_token'
-
 function getMainCategories(categContainer, selectedCateg = '') {
     let options = ''
+    let categoriesDiv = ''
 
     fetch('http://localhost:5000/all-main-categs').then(res => res.json()).then(data => {
         data.data.forEach(categ => {
-            if (categ.name == selectedCateg) {
-                options += `<option value="${categ.name}" selected>${categ.name}</option>`
-            } else {
-                options += `<option value="${categ.name}">${categ.name}</option>`
-            }
+            const { _id: categId, name: categName } = categ
+
+            fetch(`http://localhost:5000/get-subs-by-main/${categId}`).then(res => res.json()).then(data => {
+                console.log(data.data.length)
+                if (data.data.length) {
+                    if (categName == selectedCateg) {
+                        options += `<option value="${categId}" selected>${categName}</option>`
+                    } else {
+                        options += `<option value="${categId}">${categName}</option>`
+                    }
+                }
+
+                categoriesDiv = `<option value="">Choose</option>` + options
+
+                categContainer.innerHTML = categoriesDiv
+            })
         })
 
-        let categoriesDiv = ''
-
-        if (userToken) {
-            categoriesDiv = `<option value="">Choose</option>` + options
-        }
-
-        categContainer.innerHTML = categoriesDiv
     })
 }
 
--- js/scripts/user-add-book.js
+++ js/scripts/user-add-book.js
@@ -29,6 +29,9 @@ mainCategoriesContainer.addEventListener('change', e => {
 
 
 // #region add book
+// render select for main and sub
+
+
 const addBookForm = document.getElementById('add-book-form')
 const titleInput = document.getElementById('title')
 const descriptionInput = document.getElementById('description')
@@ -103,98 +106,4 @@ addBookForm.addEventListener('submit', event => {
             .catch(err => console.log(err))
     }
 })
-// #endregion add main category
-
-
-// #region add subcategory
-// let addSubcategForm = document.getElementById('add-subcategory-form')
-
-// let subcategoryNameInput = document.getElementById('subcateg-name')
-// let addSubcategBtn = document.getElementById('addSubcategBtn')
-
-// addSubcategBtn.addEventListener('click', event => {
-//     let checkCategNameReturn = checkCategName(subcategoryNameInput)
-
-//     let formData = new FormData(addSubcategForm);
-//     let data = Object.fromEntries(formData)
-//     console.log(data)
-
-//     const myHeaders = new Headers();
-
-//     const options = {
-//         method: 'POST',
-//         headers: myHeaders,
-//         body: JSON.stringify(data)
-//     }
-
-//     // delete options.headers['Content-Type'];
-//     myHeaders.append('Content-Type', 'application/json');
-//     myHeaders.append('authorization', `Bearer ${getCookie(adminToken)}`);
-
-//     if (checkCategNameReturn) {
-//         fetch('http://localhost:5000/admin/add-sub-categ', options)
-//             .then(res => {
-//                 console.log(res);
-//                 if (res.status == 200) {
-//                     deleteFormError(addSubcategForm)
-
-//                     return res.json();
-//                 } else {
-//                     setFormError(addSubcategForm, 'هذا القسم الفرعي موجود مسبقًا في القسم الأساسي الذي اخترته.', subcategoryNameInput)
-//                 }
-//             })
-//             .then(data => console.log(data))
-//             .catch(err => console.log(err))
-//     }
-
-// })
-
-// let addSubToMain = document.getElementById('addSubToMain')
-// let subcategAlert = document.querySelector('.subcateg-added')
-
-// addSubToMain.addEventListener('click', event => {
-//     let checkSelectCategReturn = checkSelectCateg(mainCategoriesContainer.querySelector('select'))
-
-//     let formData = new FormData(addSubcategForm);
-//     let data = Object.fromEntries(formData)
-//     console.log(data)
-
-//     const myHeaders = new Headers();
-
-//     const options = {
-//         method: 'POST',
-//         headers: myHeaders,
-//         body: JSON.stringify(data)
-//     }
-
-//     // delete options.headers['Content-Type'];
-//     myHeaders.append('Content-Type', 'application/json');
-//     myHeaders.append('authorization', `Bearer ${getCookie(adminToken)}`);
-
-//     const addedSuccessfullyAlert = `
-//     <div class="alert alert-warning alert-dismissable">
-//         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-//         <h4> <i class="icon fa fa-check"></i> Alert!</h4>
-//         Subcategory added successfully
-//     </div>`
-
-
-//     if (checkCategNameReturn) {
-//         fetch('http://localhost:5000/admin/add-sub-to-main', options)
-//             .then(res => {
-//                 console.log(res);
-//                 if (res.status == 200) {
-//                     deleteFormError(addSubcategForm)
-//                     subcategAlert.innerHTML = addedSuccessfullyAlert
-
-//                     return res.json();
-//                 } else {
-//                     setFormError(addSubcategForm, 'هذا القسم الفرعي موجود مسبقًا في القسم الأساسي الذي اخترته.', subcategoryNameInput)
-//                 }
-//             })
-//             .then(data => console.log(data))
-//             .catch(err => console.log(err))
-//     }
-
-// })
-    // #endregion add subcategory
+// #endregion add book
