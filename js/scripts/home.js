5b79177ff92b2a015b93818ce2ad60487090b30a
Soha <sohaali710@gmail.com>
2023-06-08 16:34:39 +0300

    display books in home page

---
+++ js/scripts/home.js
@@ -0,0 +1,34 @@
+let booksContainer = document.querySelector('.allBooksContainer')
+
+let bookUI = ''
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	fetch(`http://localhost:5000/all-books`).then(res => res.json()).then(data => {
+		console.log(data);
+		data.data.forEach(bookData => {
+			let { _id, title, subcateg, authorname, img, book } = bookData
+			img = img.replace('public', 'http://localhost:5000')
+
+			bookUI += `
+        <div class="col-xl-3 col-lg-4 col-md-4 col-12">
+								<div class="single-product">
+									<div class="product-img">
+										<a href="book-details.html?bookId=${_id}">
+											<img class="default-img" src="${img}" alt="book image" loading="lazy">
+										</a>
+									</div>
+									<div class="product-content text-center">
+										<h3><a href="book-details.html?bookId=${_id}">${title}</a></h3>
+										<div class="product-price">
+											<span>${authorname}</span>
+										</div>
+									</div>
+								</div>
+							</div>`
+		})
+
+
+		booksContainer.innerHTML = bookUI
+	});
+})
