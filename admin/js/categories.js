beede33d5849964d9e2ec8c02e720846acc2c3a7
Soha <sohaali710@gmail.com>
2023-06-08 16:38:47 +0300

    edit main category

--- admin/js/categories.js
+++ admin/js/categories.js
@@ -1,7 +1,7 @@
 import { getCookie } from '../../js/scripts/cookies.js'
 import { checkCategName, checkCategImg, setFormError, deleteFormError, checkSelectCateg, checkTextInputs, checkCheckBoxInputs } from '../../js/scripts/form-validation.js'
 import { getCategoriesAccordion } from './get-categories-accordion.js'
-import { getMainCategories } from './get-main-categories.js'
+import { getMainCategories } from './get-main-categories-checkbox.js'
 
 const adminToken = 'admin_access_token'
 
@@ -15,7 +15,7 @@ let categAlert = document.querySelector('.categ-added')
 
 // #region all main categories
 const mainCategoriesContainer = document.querySelector('.main-categories')
-let categContainer = document.querySelector('#accordion')
+let categContainer = document.querySelector('#accordionExample')
 
 document.addEventListener('DOMContentLoaded', () => {
     getMainCategories(mainCategoriesContainer)
@@ -60,12 +60,11 @@ addCategForm.addEventListener('submit', event => {
             .then(res => {
                 console.log(res);
                 if (res.status == 200) {
-                    // getCategories(filterByCategCol)
                     deleteFormError(addCategForm)
                     categAlert.innerHTML = addedSuccessfullyAlert
 
-                    // console.log(res.data.img)
-
+                    getCategoriesAccordion(categContainer)
+                    addCategForm.reset()
                     return res.json();
                 } else {
                     setFormError(addCategForm, 'هذا القسم موجود بالفعل.', categoryNameInput, categImgInput)
@@ -132,6 +131,8 @@ addSubcategForm.addEventListener('submit', event => {
                     deleteFormError(addSubcategForm)
                     subcategAlert.innerHTML = addedSuccessfullyAlert
 
+                    getCategoriesAccordion(categContainer)
+                    addSubcategForm.reset()
                     return res.json();
                 } else {
                     setFormError(addSubcategForm, 'هذا القسم الفرعي موجود مسبقًا في القسم الأساسي الذي اخترته.', subcategoryNameInput)
@@ -145,15 +146,169 @@ addSubcategForm.addEventListener('submit', event => {
 
 // #endregion add subcategory
 
-var expanded = false;
 
-function showCheckboxes() {
-    var checkboxes = document.getElementById("checkboxes");
-    if (!expanded) {
-        checkboxes.style.display = "block";
-        expanded = true;
-    } else {
-        checkboxes.style.display = "none";
-        expanded = false;
+
+// #region edit mainCateg
+let editCategForm = document.getElementById('edit-main-category-form')
+let editCategNameInput = editCategForm.querySelector('#categ-name')
+let editCategImgInput = editCategForm.querySelector('#categ-img')
+let categImg = editCategForm.querySelector('.image')
+
+categContainer.addEventListener('click', e => {
+    if (e.target.matches('.accordion-button .fa-pen-to-square')) {
+        const mainId = e.target.getAttribute('mainId')
+        console.log(mainId)
+
+        /** assign saved data to the inputs*/
+        fetch(`http://localhost:5000/main-categ-by-id/${mainId}`)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                let { name, img } = data.data
+                console.log(data.data)
+                img = img.replace('public', 'http://localhost:5000')
+
+                editCategNameInput.value = name
+                categImg.innerHTML = `<img src="${img}" alt="category image"/>`
+
+                let blob = new Blob([img], { type: "text/plain" });
+                loadURLToInputFiled(img)
+            });
+
+        /**  edit main category*/
+        editCategForm.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const checkCategNameReturn = checkCategName(editCategNameInput)
+            const checkCategImgReturn = checkCategImg(editCategImgInput)
+
+
+            const formData = new FormData(editCategForm);
+            formData.append('id', mainId)
+            const data = Object.fromEntries(formData)
+            console.log(data)
+
+            const myHeaders = new Headers();
+
+            const options = {
+                method: 'POST',
+                headers: myHeaders,
+                body: formData
+            }
+
+            delete options.headers['Content-Type'];
+            myHeaders.append('authorization', `Bearer ${getCookie(adminToken)}`);
+
+            if (checkCategNameReturn && checkCategImgReturn) {
+                fetch(`http://localhost:5000/admin/edit-main-categ`, options)
+                    .then(res => {
+                        console.log(res);
+                        if (res.status == 200) {
+                            deleteFormError(addCategForm)
+
+                            getCategoriesAccordion(categContainer)
+                            addCategForm.reset()
+                            return res.json();
+                        } else {
+                            setFormError(addCategForm, 'هذا القسم موجود بالفعل.', editCategNameInput, editCategImgInput)
+                        }
+                    })
+                    .then(data => console.log(data))
+                    .catch(err => console.log(err))
+            }
+        })
     }
+})
+// #endregion edit mainCateg
+
+// #region edit subcateg
+let editSubcategForm = document.getElementById('edit-subcategory-form')
+let editSubcategNameInput = editSubcategForm.querySelector('#categ-name')
+
+categContainer.addEventListener('click', e => {
+    if (e.target.matches('.subItem .fa-pen-to-square')) {
+        const subId = e.target.getAttribute('subId')
+        console.log(subId)
+
+        /** assign saved data to the inputs*/
+        fetch(`http://localhost:5000/get-sub-categ-by-id/${subId}`)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                let { name } = data.data
+                console.log(data.data)
+
+                editSubcategNameInput.value = name
+            });
+
+        /**  edit subcategory*/
+        editSubcategForm.addEventListener('submit', event => {
+            event.preventDefault();
+
+            const checkSubcategNameReturn = checkSubcategName(editSubcategNameInput)
+
+            const formData = new FormData(editSubcategForm);
+            formData.append('id', subId)
+            const data = Object.fromEntries(formData)
+            console.log(data)
+
+            const myHeaders = new Headers();
+
+            const options = {
+                method: 'POST',
+                headers: myHeaders,
+                body: formData
+            }
+
+            delete options.headers['Content-Type'];
+            myHeaders.append('authorization', `Bearer ${getCookie(adminToken)}`);
+
+            if (checkSubcategNameReturn) {
+                fetch(`http://localhost:5000/admin/edit-sub-categ`, options)
+                    .then(res => {
+                        console.log(res);
+                        if (res.status == 200) {
+                            deleteFormError(addSubcategForm)
+
+                            getCategoriesAccordion(categContainer)
+                            addSubcategForm.reset()
+                            return res.json();
+                        } else {
+                            setFormError(addSubcategForm, 'هذا القسم موجود بالفعل.', editSubcategNameInput)
+                        }
+                    })
+                    .then(data => console.log(data))
+                    .catch(err => console.log(err))
+            }
+        })
+    }
+})
+// #endregion edit subcateg
+
+
+
+
+// #region load img from database to input file
+function getImgURL(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
 }
+function loadURLToInputFiled(url) {
+    getImgURL(url, (blob) => {
+        // Load img blob to input
+        // WIP: UTF8 character error
+        let fileName = 'image.jpg'
+        let file = new File([blob], fileName, { type: "image/jpg", lastModified: new Date().getTime() }, 'utf-8');
+        let container = new DataTransfer();
+        container.items.add(file);
+        editCategImgInput.files = container.files;
+    })
+}
+// #endregion load img from database to input file
--- admin/js/get-categories-accordion.js
+++ admin/js/get-categories-accordion.js
@@ -1,10 +1,9 @@
 // in accordion [admin (show categories and its subcategories)]
 import { getCookie } from "../../js/scripts/cookies.js"
 
-let categoriesDiv = ''
 
 function getCategoriesAccordion(container) {
-
+  let categoriesDiv = ''
   fetch('http://localhost:5000/all-main-categs').then(res => res.json()).then(data => {
     data.data.forEach(categ => {
       const { _id: categId, name: categName } = categ
@@ -15,45 +14,53 @@ function getCategoriesAccordion(container) {
 
         if (subcategArr.length) {
           subcategArr.forEach(subcateg => {
-            const { name: subName } = subcateg
+            const { _id: subcategId, name: subName } = subcateg
 
-            subcategLis += `<li>${subName}</li>`
+            subcategLis += `
+                      <li class="subItem">
+                        ${subName}
+                        <i class="fa-solid fa-pen-to-square" subId="${subcategId}" data-bs-toggle="modal" data-bs-target="#exampleModal1"></i>
+                        <i class="fa-solid fa-xmark" subId="${subcategId}"></i>
+                      </li>`
 
           })
 
           categoriesDiv += `
-                          <div class="panel box box-primary">
-                          <div class="box-header with-border">
-                            <h4 class="box-title">
-                              <a data-toggle="collapse" data-parent="#accordion" href="#${categName}">
-                                ${categName}
-                              </a>
-                            </h4>
-                          </div>
-                          <div id="${categName}" class="panel-collapse collapse">
-                            <div class="box-body">
-                            <ul>`
+          <div class="accordion-item">
+            <h2 class="accordion-header" id="${categName}">
+              <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#header${categName}" aria-expanded="false" aria-controls="header${categName}">
+              ${categName}
+              <!--Button trigger modal[edit categ]-->
+                <i class="fa-solid fa-pen-to-square" mainId="${categId}" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
+                <i class="fa-solid fa-xmark" mainId="${categId}"></i>
+              </button>
+            </h2 >
+            <div id="header${categName}" class="accordion-collapse collapse" aria-labelledby="${categName}" data-bs-parent="#accordionExample">
+              <div class="accordion-body">
+                <ul>`
             + subcategLis +
             `</ul>
-                            </div>
-                          </div>
-                        </div>`
+              </div>
+            </div>
+          </div >`
         } else {
           categoriesDiv += `
-                          <div class="panel box box-primary">
+                        <div class="panel box box-primary">
                           <div class="box-header with-border">
                             <h4 class="box-title">
                               <a data-toggle="collapse" data-parent="#accordion" href="#${categName}">
                                 ${categName}
                               </a>
                             </h4>
+                            <i class="fa-solid fa-xmark" mainId="${categId}"></i>
+                              <i class="fa-solid fa-pen-to-square" mainId="${categId}"></i>
                           </div>
                           <div id="${categName}" class="panel-collapse collapse">
                             <div class="box-body">
                               <p class="text-center">There is no subcategories here</p>
                             </div>
                           </div>
-                        </div>`
+                        </div> `
         }
 
         container.innerHTML = categoriesDiv
